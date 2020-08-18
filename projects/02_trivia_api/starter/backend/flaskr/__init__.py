256ca2c92adb8c7e2ea462548201a088fc12b559
Tawfiek <tawfiek.108@gmail.com>
2020-08-18 02:13:49 +0200

    feat: add all required routes

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -8,97 +8,133 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
-  
-  '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
+    # create and configure the app
+    app = Flask(__name__)
+    db = setup_db(app)
 
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
 
+    @app.route('/categories')
+    def get_all_categories():
+        categories = Category.query.all()
+        formatted_categories = {categories[i].id: categories[i].type for i in range(0, len(categories), 1)}
+        return jsonify({
+            'success': True,
+            'categories': formatted_categories
+        })
 
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
+    @app.route('/questions', methods=['GET', 'POST'])
+    def get_all_questions():
+        if request.method == 'GET':
+            page = request.args.get('page', default=1, type=int)
+            questions = Question.query.all()
+            formatted_questions = [q.format() for q in questions]
+            categories = Category.query.all()
 
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
+            formatted_categories ={categories[i].id: categories[i].type for i in range(0, len(categories), 1)}
+            start = (page-1) * 10
+            end = page * 10
+            return jsonify({
+                'success': True,
+                'questions': formatted_questions[start:end],
+                'total_questions': len(formatted_questions),
+                'categories': formatted_categories,
+                'current_category': 1
+            })
+        elif request.method == 'POST':
+            try:
+                body = request.get_json()
 
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
+                q = Question(question=body['question'],
+                             answer=body['answer'],
+                             category=body['category'],
+                             difficulty=body['difficulty'])
 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
+                db.session.add(q)
+                db.session.commit()
+                return jsonify(q.format())
+            except:
+                db.session.rollback()
+            finally:
+                db.session.close()
 
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        try:
+            Question.query.filter_by(id=question_id).delete()
+            db.session.commit()
+        except:
+            db.session.rollback()
+        finally:
+            db.session.close()
+        return jsonify({'success': True})
 
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
+    @app.route('/questions-search', methods=['POST'])
+    def search_questions():
+        body = request.get_json()
+        search_term = body['searchTerm']
+        search = "%{}%".format(search_term.lower())
+        questions = Question.query\
+            .filter(db.func.lower(Question.question).like(search)).all()
+        formatted_questions = [question.format() for question in questions]
+        return jsonify({
+            'questions': formatted_questions,
+            'totalQuestions': len(formatted_questions),
+            'currentCategory': 1
+        })
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
+    @app.route('/categories/<int:cat_id>/questions')
+    def get_questions_by_cat_id(cat_id):
+        questions = Question.query.filter_by(category=cat_id).all()
+        formatted_questions = [question.format() for question in questions]
+        return jsonify({
+            'questions': formatted_questions,
+            'totalQuestions': len(formatted_questions),
+            'currentCategory': cat_id
+        })
 
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
+    @app.route('/quizzes', methods=['POST'])
+    def play_quiz():
+        body = request.get_json()
+        category = body['quiz_category']
+        previous_questions = body['previous_questions']
+        res_boy = get_random_question(category['id'], previous_questions)
+        previous_questions.append(res_boy['question']['id'])
 
-  '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
+        return jsonify(res_boy)
 
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
+    def get_random_question(cat_id: int, prev_questions):
+        if cat_id > 0:
+            questions = Question.query.order_by('id')\
+                .filter_by(category=cat_id).all()
+        else:
+            questions = Question.query.order_by('id').all()
 
+        formatted_questions = [q.format() for q in questions]
+        no_of_questions = len(formatted_questions)
+        number = random.randint(0, no_of_questions - 1)
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
+        if len(prev_questions) < no_of_questions:
+            for p in prev_questions:
+                while formatted_questions[number]['id'] == p:
+                    number = random.randint(0, no_of_questions - 1)
 
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
-
-  '''
+        return {
+                'question': formatted_questions[number],
+                'forceEnd': no_of_questions == len(prev_questions) +1
+            }
+    ''' 
   @TODO: 
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
-  
-  return app
 
-    
+    return app
--- projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
+++ projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
@@ -23,9 +23,10 @@ class QuestionView extends Component {
 
   getQuestions = () => {
     $.ajax({
-      url: `/questions?page=${this.state.page}`, //TODO: update request URL
+      url: `/questions?page=${this.state.page}`,
       type: "GET",
       success: (result) => {
+        console.log('#DEBUG RESULTS ', result)
         this.setState({
           questions: result.questions,
           totalQuestions: result.total_questions,
@@ -60,7 +61,7 @@ class QuestionView extends Component {
 
   getByCategory= (id) => {
     $.ajax({
-      url: `/categories/${id}/questions`, //TODO: update request URL
+      url: `/categories/${id}/questions`,
       type: "GET",
       success: (result) => {
         this.setState({
@@ -78,7 +79,7 @@ class QuestionView extends Component {
 
   submitSearch = (searchTerm) => {
     $.ajax({
-      url: `/questions`, //TODO: update request URL
+      url: `/questions-search`,
       type: "POST",
       dataType: 'json',
       contentType: 'application/json',
@@ -105,7 +106,7 @@ class QuestionView extends Component {
     if(action === 'DELETE') {
       if(window.confirm('are you sure you want to delete the question?')) {
         $.ajax({
-          url: `/questions/${id}`, //TODO: update request URL
+          url: `/questions/${id}`,
           type: "DELETE",
           success: (result) => {
             this.getQuestions();
@@ -141,7 +142,7 @@ class QuestionView extends Component {
               key={q.id}
               question={q.question}
               answer={q.answer}
-              category={this.state.categories[q.category]} 
+              category={this.state.categories[q.category]}
               difficulty={q.difficulty}
               questionAction={this.questionAction(q.id)}
             />
