2b636a35f166583d940c86761c2fcb53775e26e3
Tawfiek <tawfiek.108@gmail.com>
2020-08-25 03:26:05 +0200

    tests: adding tests for all APIs

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}/{}".format('root:123@localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,15 +24,84 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
+    def test_categories_route(self):
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsNotNone(data['categories'])
+        self.assertGreater(len(data['categories']), 0)
+
+    def test_questions_route(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertIsNotNone(data['categories'])
+        self.assertTrue(len(data['categories']))
+        self.assertIsNotNone(data['questions'])
+        self.assertGreater(len(data['questions']), 0)
+        self.assertGreater(len(data['questions']), 0)
+
+    def test_post_questions_route(self):
+        req_body = {
+            'question': 'whats app ?',
+            'answer': 'I am fine',
+            'category': '1',
+            'difficulty': '2'
+        }
+        res = self.client().post('/questions', json=req_body)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_delete_questions_route(self):
+        res = self.client().delete('/questions/1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_delete_questions_route(self):
+        res = self.client().delete('/questions/1')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    def test_search_questions(self):
+        res = self.client().post('/questions-search', json={'searchTerm': 'w'})
+
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertGreater(len(data['questions']), 0)
+        self.assertEqual(len(data['questions']), data['totalQuestions'])
+
+    def test_get_questions_by_cat_id(self):
+        res = self.client().get('/categories/1/questions')
+
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertGreater(len(data['questions']), 0)
+        self.assertEqual(len(data['questions']), data['totalQuestions'])
+        self.assertEqual(data['currentCategory'], 1)
+
+    def test_play_quiz(self):
+        res = self.client().get('/categories/1/questions')
+
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertIsNotNone(data['question'])
+
 
 
 # Make the tests conveniently executable
